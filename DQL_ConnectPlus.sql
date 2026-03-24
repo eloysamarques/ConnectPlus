7c583eda25a1b7a5d997f4f418ee4386e898881a
ELOYSA MARQUES DOS SANTOS <7811003309@info.local>
2026-03-24 16:51:35 -0300

    Começado API ConnectPlus

---
+++ DQL_ConnectPlus.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE ConnectPlus;
+GO
+
+USE ConnectPlus;
+
+CREATE TABLE TipoContato
+(
+    IdTipoContato UNIQUEIDENTIFIER PRIMARY KEY DEFAULT ((NEWID())),
+    Titulo        VARCHAR(100)     NOT NULL
+);
+SELECT * FROM TipoContato
+GO
+
+CREATE TABLE Contato
+(
+    IdContato      UNIQUEIDENTIFIER PRIMARY KEY DEFAULT ((NEWID())),
+    Nome           VARCHAR(150) NOT NULL,
+    FormaContato   VARCHAR(150) NOT NULL,
+    Imagem         VARCHAR(255),
+   
+    IdTipoContato  UNIQUEIDENTIFIER NOT NULL FOREIGN KEY REFERENCES TipoContato(IdTipoContato)
+);
+SELECT * FROM Contato
+GO
